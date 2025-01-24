e18444fd967228826aeb4cac5efb650140d54734
Vlad Rutskiy <vladrytskiy@gmail.com>
2025-01-24 11:44:02 -0700

    added a program which asks user to enter numbers

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
-print("hello, man")
-print("developer is ok!")
-x=1
-y=2
-z=x+y
-print(z)
+print("hello, I'm going to ask you to print two numbers…")
+print("I hope you are OK with that")
+x = input("Please enter first number: ")
+y = input("Please enter second number: ")
+print(type(y))
+print(type(x))
+newX = int(x)
+newY = int(y)
+print(type(newX))
+print(type(newY))
+z = newX + newY
+w = newX * newY
+print("I added both numbers, and the result is: ", z)
+print("I multiplied both numbers, and the result is: ", w)
