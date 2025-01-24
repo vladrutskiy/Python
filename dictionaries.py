ef3cfede4cfcc5aa6658772408ed4736bc7a4bae
Vlad Rutskiy <vladrytskiy@gmail.com>
2025-01-23 19:33:46 -0700

    added main.py file

---
+++ dictionaries.py
@@ -0,0 +1,24 @@
+#inline (Single line) comment
+# hello
+# This 
+# is 
+# ammout
+
+"""And this is is for multiple
+lines quatation
+"""
+
+pets_love = {
+    "Rosie": {
+        "Cuteness": 10,
+        "Smarts": 5,
+        "Attitude": 8,
+        "Luck": 5
+    }
+}
+pets_love["Elmer"] = 11
+pets_love["Sherman"] = 8
+
+pets_love["Sherman"] = 2
+print("Joel loves Sherman", pets_love["Sherman"])
+print("Rosie's smartness is ", pets_love["Rosie"]["Smarts"])
---
+++ main.py
@@ -0,0 +1,5 @@
+numbers = { 
+    "four":4, "three":3, "two":2
+    }
+numbers["five"] = 5
+print(numbers)
