d78844413872a053e99e88374546e6ce8a0bb938
Vlad Rutskiy <vladrytskiy@gmail.com>
2025-02-12 20:03:35 -0700

    added two files

---
+++ training/selenium_web_driver/CSS_selector.py
@@ -0,0 +1,5 @@
+#  sometimes the id's are dynamic to catch them there are several ways
+
+# if the prefix is stable but the ending is changing use ˆ sign  (alt + I) like this : input [idˆ= "dl_lkj_ "]
+# if the suffix is stable  use the $ sign like this : input [id$ = "_df_lku_"]
+# if the sub-string is stable use * sign like this : input [id* = "_df_lku_"]
