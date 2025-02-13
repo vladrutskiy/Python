ed2c5e8837c238eafd9560cb05a7fdd1cf3e4633
Vlad Rutskiy <vladrytskiy@gmail.com>
2025-02-13 14:24:33 -0700

    1. Added CSS selector hint file 2. Added Xpath selector hint file 3. Added two almost useless pdf file for jupyter notebook scripting

--- training/selenium_web_driver/CSS_selector.py
+++ training/selenium_web_driver/CSS_selector.py
@@ -1,5 +1,51 @@
+# ID selection
+"""
+looks like:  #username
+"""
+
+# Class adn other elements selection
+"""
+to choose class use dot: .hidden
+or use it like this: input[name="username"] or input[class="hidden"]
+"""
+
+# Combining attributes to find unique element
+"""
+label[class="bold"][id="username"] - you can add numerous elements in []
+"""
+
+# Combining several attributes to find on the page using class and id
+"""
+we are using label with class and id here
+label.bold#username
+"""
+
 #  sometimes the id's are dynamic to catch them there are several ways
 
-# if the prefix is stable but the ending is changing use ˆ sign  (alt + I) like this : input [idˆ= "dl_lkj_ "]
-# if the suffix is stable  use the $ sign like this : input [id$ = "_df_lku_"]
-# if the sub-string is stable use * sign like this : input [id* = "_df_lku_"]
+# if the prefix is stable but the ending is changing 
+"""
+use ˆ sign  (alt + I) like this: input [idˆ= "dl_lkj_ "]
+"""
+# if the suffix is stable  
+"""
+use the $ sign like this: input [id$ = "_df_lku_"]
+"""
+
+# if the sub-string is stable 
+"""
+use * sign like this: input [id* = "_df_lku_"]
+"""
+
+# Next sibling selection
+"""
+looks like: select#month > option + option + option   this will help to select third month.
+"""
+
+# Pseudo Classes
+""""
+Pseudo classes: select#month :first-child 
+to select other child you can use select#month :nth-child(7) 
+to start selecting from the other side use elect#month :nth-last-child(7)
+"""
+
+
---
+++ training/selenium_web_driver/xpath_selector.py
@@ -0,0 +1,20 @@
+# General XPATH 
+"""
+Xpath=//tagname[@Attribute="value"]
+/html/body/div/h1
+/html/body/div[18]/div/div/form/div[1]/div[1]/input
+"""
+
+# ID or class selection
+"""
+//*[@id="about"]  //*[@class="firstname"] //span[@id="nameError"]
+"""
+
+# Combining several attributes
+"""
+//input[@id="name"][@name="name"][@placeholder="John"] 
+"""
+
+# ID selection
+"""
+"""
