82e2ba9cb681cbab205b4e0f92ee5c5243f64496
Vlad Rutskiy <vladrytskiy@gmail.com>
2025-03-25 15:53:30 -0600

    added some xpath functions

--- training/selenium_web_driver/basics/xpath_functions.py
+++ training/selenium_web_driver/basics/xpath_functions.py
@@ -13,16 +13,40 @@ syntax is:
 /html/body/div[@id="projects"]/div/div/div/p/i[contains(@class,'fa')]
 """
 
-# ID selection
+# to find dynamic elements using contains
 """
+syntax is:
+//tagname[contains(@attribute,"value")]
+
+//button[contains(@id,"342")] actual search button at our Salesforce page
 """
 
-# ID selection
+# to find an element with exact text match use text function
 """
+syntax is:
+//tagname[text()='value']
+
+//div[text()="New"][@title="New"]
 """
 
-# ID selection
+# To find an element using more that one attribute use "and" & "or" functions
 """
+//tagname[@attribute1="value1" and @attribute2="value2"]
+//tagname[@attribute1="value1" or @attribute2="value2"]
+
+//a[@title="New" or @title="Intelligence View" and @role="button"]
+
+"""
+
+# xpath axes methods to find elements (parent, child, self, sibling, ancestor, descendant)
+"""
+//tagname[@attribute="value"]//parent::tagname
+//tagname[@attribute="value"]//child::tagname
+//tagname[@attribute="value"]//self::tagname
+//tagname[@attribute="value"]//sibling::tagname
+//tagname[@attribute="value"]//ancestor::tagname
+//tagname[@attribute="value"]//descendant::tagname 
+
 """
 
 # ID selection
