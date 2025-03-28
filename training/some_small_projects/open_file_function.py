1ef6225b93ec5d61c23fd264e47133d9c9d9f4fd
Vlad Rutskiy <vladrytskiy@gmail.com>
2025-03-28 10:41:21 -0600

    some small changes

--- training/some_small_projects/open_file_function.py
+++ training/some_small_projects/open_file_function.py
@@ -1,6 +1,6 @@
 # to open large file (pdf, xlsx) use pandas
 # to open simple files like txt use open() and "r" mode
-file =  open("./training/simple_text_file.txt" , "r")
+file =  open("./training/some_small_projects/simple_text_file.txt" , "r")
 print(file)
 print(file.read())
 file.close()
@@ -11,13 +11,13 @@ file.close()
 # file.close()
 
 # appending the file with new data ("a" mode)
-file = open("./training/simple_text_file.txt", "a")
+file = open("./training/some_small_projects/simple_text_file.txt", "a")
 file.write("No it is me who is adding a new line \n")
 file.close()
 
 # to read and write to file use "a+" mode
 # .seek function starts reading form the beginning of the file
-file = open("./training/simple_text_file.txt", "a+")
+file = open("./training/some_small_projects/simple_text_file.txt", "a+")
 file.seek(0)
 print(file.read())
 file.write("Me too \n")
